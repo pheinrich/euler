dc97e42eded34709bbfd9458e4b49fc8e6048fee
Peter Heinrich <peter.heinrich@gmail.com>
2013-01-23 12:44:17 -0800

    Added placeholder for problem 5

---
+++ ProjectEuler/problem_5.rb
@@ -0,0 +1,17 @@
+require 'ProjectEuler'
+
+class Problem_5
+  # 2520 is the smallest number that can be divided by each of the numbers
+  # from 1 to 10 without any remainder.
+  #
+  # What is the smallest positive number that is evenly divisible by all of
+  # the numbers from 1 to 20?
+  
+  def self.solve
+    puts "?"
+  end
+end
+
+ProjectEuler.time do
+  Problem_5.solve
+end
