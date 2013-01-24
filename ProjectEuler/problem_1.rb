5422da4c2e34f1257046aa1254dc8e234dd4bd2e
Peter Heinrich <peter.heinrich@gmail.com>
2013-01-23 19:11:55 -0800

    Fixed require in first problem Fixed string format in time utility

--- ProjectEuler/problem_1.rb
+++ ProjectEuler/problem_1.rb
@@ -1,4 +1,4 @@
-require 'ProjectEuler'
+require 'projectEuler'
 
 class Problem_1
   # If we list all the natural numbers below 10 that are multiples of 3 or 5,
--- ProjectEuler/projectEuler.rb
+++ ProjectEuler/projectEuler.rb
@@ -2,6 +2,6 @@ module ProjectEuler
   def self.time
     start = Time.now.to_f
     yield
-    puts "%.10f%c" % [Time.now.to_f - start, 's']
+    puts "%.10f%s" % [Time.now.to_f - start, 's']
   end
 end
