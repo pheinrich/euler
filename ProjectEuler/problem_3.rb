e8b7901fada82623679497a95fe22c5465bf83f9
Peter Heinrich <peter.heinrich@gmail.com>
2013-01-23 20:21:56 -0800

    Factored prime factorization out into a utility function

--- ProjectEuler/problem_3.rb
+++ ProjectEuler/problem_3.rb
@@ -6,25 +6,7 @@ class Problem_3
   # What is the largest prime factor of the number 600851475143?
 
   def self.solve( n )
-    f = n
-    i = 2
-
-    # Divide by prime numbers.  Stop when n has gotten too small to divide by
-    # the latest prime (all smaller primes will have already been used).
-    while i <= n do
-
-      while n % i == 0 do
-        # If the current prime is a factor, keep factoring it out as long as
-        # possible.  Non-prime multiples of previous factors won't get here.     
-        f = i
-        n /= i
-      end
-
-      # Advance to the next candidate. 
-      i += 1
-    end
-
-    puts 1 < n ? n : f
+    puts ProjectEuler.prime_factors( n )[-1]
   end
 end
 
--- ProjectEuler/projectEuler.rb
+++ ProjectEuler/projectEuler.rb
@@ -4,4 +4,25 @@ module ProjectEuler
     yield
     puts "%.10f%s" % [Time.now.to_f - start, 's']
   end
+  
+  def self.prime_factors( n )
+    arr = []
+    i = 2
+
+    # Divide by prime numbers.  Stop when n has gotten too small to divide by
+    # the latest prime (all smaller primes will have already been used).
+    while i <= n do
+      while n % i == 0 do
+        # If the current prime is a factor, keep factoring it out as long as
+        # possible.  Non-prime multiples of previous factors won't get here.     
+        arr << i
+        n /= i
+      end
+
+      # Advance to the next candidate. 
+      i += 1
+    end
+
+    arr
+  end
 end
