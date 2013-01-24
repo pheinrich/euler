d1e5ec57eacbe1c92daf821b2d6ac2ce7aeb58ff
Peter Heinrich <peter.heinrich@gmail.com>
2013-01-24 12:28:57 -0800

    Completed problem 6

--- ProjectEuler/problem_6.rb
+++ ProjectEuler/problem_6.rb
@@ -10,16 +10,19 @@ class Problem_6
   #      (1 + 2 + ... + 10)^2 = 55^2 = 3025
   #
   # Hence the difference between the sum of the squares of the first ten
-  # natural numbers and the square of the sum is 3025  385 = 2640.
+  # natural numbers and the square of the sum is 3025 - 385 = 2640.
   #
   # Find the difference between the sum of the squares of the first one
   # hundred natural numbers and the square of the sum.
-  
-  def self.solve
-    puts "?"
+
+  def self.solve( n )
+    # Sum of squares is given by n(n + 1)(2n + 1) / 6, while square of sums
+    # is [n(n + 1)][n(n + 1)] / 4.  Subtracting and simplifying the result
+    # gives n(n + 1)(n - 1)(3n + 2) / 12.
+    puts n * (n + 1) * (n - 1) * (3*n + 2) / 12
   end
 end
 
 ProjectEuler.time do
-  Problem_6.solve()
+  Problem_6.solve( 100 )
 end
