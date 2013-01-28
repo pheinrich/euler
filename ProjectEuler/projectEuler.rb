893279f99f09fa5fc71f1789580f240d81915e40
Peter Heinrich <peter.heinrich@gmail.com>
2013-01-27 17:33:25 -0800

    Updated comments for a few utility methods

--- ProjectEuler/projectEuler.rb
+++ ProjectEuler/projectEuler.rb
@@ -4,7 +4,8 @@ module ProjectEuler
     yield
     puts "%.10f%s" % [Time.now.to_f - start, 's']
   end
-  
+
+  # Return an array of all divisors of a number.
   def self.factors( n )
     arr = [1]
     max = Math.sqrt( n ).to_i
@@ -13,6 +14,8 @@ module ProjectEuler
     arr |= arr.map {|x| n / x}
   end
 
+  # Return an array of all prime numbers that divide a number.  Entries may be
+  # duplicated (e.g. 234 = 2 x 3 x 3 x 13).
   def self.prime_factors( n )
     arr = []
     i = 2
@@ -35,6 +38,7 @@ module ProjectEuler
     arr
   end
 
+  # Returns true if a number is prime, otherwise false.
   def self.prime?( n )
     return true if 2 == n
     return false if 2 > n || 0 == n % 2
@@ -52,7 +56,8 @@ module ProjectEuler
     true
   end
 
-  # Use the Sieve of Eratosthenes to initialize an array prime numbers.
+  # Return an array of prime numbers less than the maximum specified.  Use the
+  # Sieve of Eratosthenes to generate the array.
   def self.eratosthenes( n )
     s = Array.new( n ) {|i| i}
     s[0] = s[1] = nil
@@ -74,6 +79,7 @@ module ProjectEuler
     (1..e).inject( 1 ) {|c| (c * b) % m}
   end
 
+  # Return the Collatz sequence for the specified number.
   def self.collatz( n )
     arr = [n]
     while 1 != n
@@ -84,6 +90,7 @@ module ProjectEuler
     arr
   end
 
+  # Return the length the Collatz sequence associated with a number.
   def self.collatz_length( n )
     l = 1
     while 1 != n
