25a65c017a84cb8509d306538845b4c4d939c0f1
Peter Heinrich <peter.heinrich@gmail.com>
2013-01-30 22:12:47 -0800

    Completed problem 15

--- ProjectEuler/problem_0015.rb
+++ ProjectEuler/problem_0015.rb
@@ -12,7 +12,19 @@ class Problem_0015
   # How many routes are there through a 20 x 20 grid?
 
   def self.solve( n )
-    puts "?"
+    # Working backward from the lower right point, it's clear that its two
+    # neighbors have only one path to reach it.  Their other common neighbor,
+    # however can reach it through either of them, so it has 1 + 1 = 2 paths
+    # to the corner point.  Similarly, every point P has M paths to the lower
+    # right, where M is the sum of paths available to P's neighbors closer to
+    # that point.  Labelling each point with its count of available paths, we
+    # see that the values form an inverted Pascal Triangle.
+    #
+    # Since Pascal's Triangle can be formed through binomial expansion, we
+    # must simply calculate the appropriate coefficient to find the solution.
+    # If a[i] are the coefficients of row n, then a[k] = nCk, or "n Choose k",
+    # where nCk = n! / (k! * (n - k)!).
+    puts( (n << 1).fact / (n.fact * n.fact) )
   end
 end
 
