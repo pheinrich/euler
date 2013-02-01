8d571bad4f6558c30e8174348afbcab368a23c04
Peter Heinrich <peter.heinrich@gmail.com>
2013-02-01 01:28:40 -0800

    Completed problems 16 and 20

---
+++ problem_0020.rb
@@ -0,0 +1,20 @@
+require 'projectEuler'
+
+# Factorial digit sum
+class Problem_0020
+  # n! means n x (n - 1) x ... x 3 x 2 x 1
+  #
+  # For example, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800, and the sum of the
+  # digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+  #
+  # Find the sum of the digits in the number 100!
+
+  def self.solve( n )
+    puts n.fact.sum_digits
+  end
+end
+
+ProjectEuler.time do
+  # 648
+  Problem_0020.solve( 100 )
+end
