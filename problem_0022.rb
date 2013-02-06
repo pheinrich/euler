54749ad99db972fb6b51cc94bd550c4ad8b9e79e
Peter Heinrich <peter.heinrich@gmail.com>
2013-02-05 23:19:36 -0800

    Completed problem 22

---
+++ problem_0022.rb
@@ -0,0 +1,31 @@
+require 'projectEuler'
+
+# Names scores
+class Problem_0022
+  # Using names.txt, a 46K text file containing over five-thousand first names,
+  # begin by sorting it into alphabetical order. Then working out the
+  # alphabetical value for each name, multiply this value by its alphabetical
+  # position in the list to obtain a name score.
+  #
+  # For example, when the list is sorted into alphabetical order, COLIN, which
+  # is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So,
+  # COLIN would obtain a score of 938 x 53 = 49714.
+  #
+  # What is the total of all the name scores in the file?
+
+  def self.solve
+    # Strip quotes and split comma-separated values into a sorted array.
+    names = IO.read( 'names.txt' ).delete( '\"' ).split( ',' ).sort
+
+    # Add all character values together, then account for offset value of 'A'.
+    names.map! {|n| n.bytes.inject( :+ ) - (n.length << 6)}
+
+    # Weight each name score according to its list position and add together.
+    puts names.each_with_index.inject( 0 ) {|sum, (n, i)| sum + n*(1 + i)}
+  end
+end
+
+ProjectEuler.time do
+  # 871198282
+  Problem_0022.solve
+end
