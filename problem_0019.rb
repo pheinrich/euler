25670b2df205b8c7bbc2b5975d5b67bd3e9ee890
Peter Heinrich <peter.heinrich@gmail.com>
2013-02-05 20:03:17 -0800

    Completed problem 19

--- problem_0019.rb
+++ problem_0019.rb
@@ -1,3 +1,4 @@
+require 'date'
 require 'projectEuler'
 
 # Counting Sundays
@@ -18,11 +19,23 @@ class Problem_0019
   # How many Sundays fell on the first of the month during the twentieth
   # century (1 Jan 1901 to 31 Dec 2000)?
 
-  def self.solve()
+  def self.solve( start, stop )
+    total = 0
+    
+    # Move to the first Sunday in the range, if not there already.
+    start += (7 - start.wday) if !start.sunday?
+
+    # Move forward a week at a time, counting the first-of-months we see.
+    while start <= stop
+      total += 1 if 1 == start.day
+      start += 7
+    end
+
+    puts total
   end
 end
 
 ProjectEuler.time do
-  # 
-  Problem_0019.solve()
+  # 171
+  Problem_0019.solve( Date.new( 1901, 1, 1 ), Date.new( 2000, 12, 31 ) )
 end
