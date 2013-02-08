d2a12c69de66e2116b08ea7fe96726acbded4084
Peter Heinrich <peter.heinrich@gmail.com>
2013-02-07 22:22:58 -0800

    Completed problem 26

--- problem_0026.rb
+++ problem_0026.rb
@@ -22,10 +22,34 @@ class Problem_0026
   # cycle in its decimal fraction part.
 
   def self.solve( n )
+    # Divide 1 by every number up to n, recording the length of repeating
+    # digits that repeat.
+    lens = (2...n).map do |i|
+      d = []
+      r = 1
+
+      # Do long division, stopping only if the decimal terminates or we see
+      # a remainder we've seen before (indicating the beginning of a cycle).
+      while true
+        r = 10 * (r % i)
+        break if 0 == r || d.include?( r )
+
+        # No cycle, but it didn't terminate.  Add the remainder and calculate
+        # a new one.
+        d << r
+        r = r % i
+      end
+
+      d.length
+    end
+
+    # Return the index of the largest length value, offset to account for the
+    # fact that we didn't divide by 0 or 1.
+    puts 2 + lens.each_with_index.max[1]
   end
 end
 
 ProjectEuler.time do
-  # 
+  # 983
   Problem_0026.solve( 1000 )
 end
