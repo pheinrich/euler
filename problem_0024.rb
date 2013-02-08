b084b3de099dbad69785cd73ff6896a58e15948f
Peter Heinrich <peter.heinrich@gmail.com>
2013-02-08 00:13:35 -0800

    Completed problem 24

--- problem_0024.rb
+++ problem_0024.rb
@@ -13,10 +13,12 @@ class Problem_0024
   # 4, 5, 6, 7, 8 and 9?
 
   def self.solve( n )
+    # Worst kind of brute force...
+    puts %w(0 1 2 3 4 5 6 7 8 9).permutation( 10 ).to_a.map! {|a| a.join}.sort![n - 1]
   end
 end
 
 ProjectEuler.time do
-  # 
+  # 2783915460
   Problem_0024.solve( 1000000 )
 end
