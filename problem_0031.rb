eacf93a1febe3bbd4e009304377d6a34952c18c5
Peter Heinrich <peter.heinrich@gmail.com>
2013-02-14 04:23:40 -0800

    Completed problem 31

--- problem_0031.rb
+++ problem_0031.rb
@@ -9,14 +9,28 @@ class Problem_0031
   #
   # It is possible to make £2 in the following way:
   #
-  #     1£1 + 150p + 220p + 15p + 12p + 31p
+  #     1x£1 + 1x50p + 2x20p + 1x5p + 1x2p + 3x1p
+  #
   # How many different ways can £2 be made using any number of coins?
 
-  def self.solve( n )
+  def self.fit( n, denoms, sum = 0, index = 0, total = 0 )
+    if index < denoms.length
+      0.step( n - sum, denoms[index]) do |x|
+        total = fit( n, denoms, x + sum, 1 + index, total )
+      end
+    elsif sum == n
+      total += 1
+    end
+
+    total
+  end
+
+  def self.solve( n, denoms )
+    puts fit( n, denoms )
   end
 end
 
 ProjectEuler.time do
-  # 
-  Problem_0031.solve( 200 )
+  # 73682
+  Problem_0031.solve( 200, [200, 100, 50, 20, 10, 5, 2, 1] )
 end
