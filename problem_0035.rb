686f3f816bb91de135ce10c2cc0788544715493e
Peter Heinrich <peter.heinrich@gmail.com>
2013-02-15 06:14:44 -0800

    Completed problem 35

--- problem_0035.rb
+++ problem_0035.rb
@@ -11,10 +11,28 @@ class Problem_0035
   # How many circular primes are there below one million?
 
   def self.solve( n )
+    total = 0
+
+    (2...n).each do |i|
+      str = i.to_s.chars.to_a
+      prime = true
+
+      # Rotate every digit into the first position as long as the resulting
+      # number continues to be prime.
+      str.length.times do
+        prime = str.join.to_i.prime?
+        break if !prime
+        str.rotate!
+      end
+
+      total += 1 if prime
+    end
+
+    puts total
   end
 end
 
 ProjectEuler.time do
-  # 
+  # 55
   Problem_0035.solve( 1000000 )
 end
