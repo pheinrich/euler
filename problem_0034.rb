bcb9350f5f204b5bc82755c6be49898fb1d1b4e8
Peter Heinrich <peter.heinrich@gmail.com>
2013-02-15 05:41:29 -0800

    Completed problem 34

--- problem_0034.rb
+++ problem_0034.rb
@@ -10,10 +10,19 @@ class Problem_0034
   # Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 
   def self.solve()
+    f = (0..9).map(&:fact)
+
+    # Determine the largest number of digits possible with the powers given.
+    lim = 0
+    lim += 1 while 10**lim - 1 <= f[9] * lim
+    lim = 10**lim - 1
+
+    # Brute force every number with that many digits or fewer.
+    puts (3..lim).select {|i| i == i.to_s.chars.inject( 0 ) {|a, x| a + f[x.to_i]}}.reduce( :+ )
   end
 end
 
 ProjectEuler.time do
-  # 
+  # 40730
   Problem_0034.solve()
 end
