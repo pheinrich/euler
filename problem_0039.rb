3152ea5663ee4bcf30441aabe62a254fe1f84b67
Peter Heinrich <peter.heinrich@gmail.com>
2013-03-06 12:38:22 -0800

    Completed problem 39

--- problem_0039.rb
+++ problem_0039.rb
@@ -10,10 +10,44 @@ class Problem_0039
   # For which value of p <= 1000, is the number of solutions maximised?
 
   def self.solve( n )
+    count = Array.new( 1 + n, 0 )
+    u, v, k = 1, 2, 1
+
+    while true
+      a = k*(v*v - u*u)
+      b = k*(2*u*v)
+      c = k*(v*v + u*u)
+
+      p = a + b + c
+
+      if n >= p
+        # If p isn't too big, increment its popularity.
+        count[p] += 1
+        k += 1
+      else
+        if k > 1
+          # If p is too big for the current k, advance v.
+          v += 2
+          k = 1
+        elsif v > u + 1
+          # If p is too big for the current v, advance u.
+          u += 1
+          v = u + 1
+        else
+          # If p is too big for the current u, we're done.
+          break
+        end
+
+        # Skip invalid triples.
+        v += 2 while !v.coprime?( u )
+      end
+    end
+
+    puts "count(%d) = %d" % [count.index( count.max ), count.max]
   end
 end
 
 ProjectEuler.time do
-  # 
+  # count(840) = 8
   Problem_0039.solve( 1000 )
 end
