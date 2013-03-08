eaceed164d0086e11c1e761d8609b8ece824f448
Peter Heinrich <peter.heinrich@gmail.com>
2013-03-08 12:05:06 -0800

    Completed problem 38

--- problem_0038.rb
+++ problem_0038.rb
@@ -19,10 +19,28 @@ class Problem_0038
   # the concatenated product of an integer with (1,2, ... , n) where n > 1?
 
   def self.solve()
+    d = %w(1 2 3 4 5 6 7 8 9).permutation.map {|i| i.join}.select {|i| i >= '918273645'}.reverse
+ 
+    d.each do |i|
+      1.upto( 4 ) do |j|
+        s = i[0, j]
+        n = s.to_i
+
+        2.upto( 5 ) do |k|
+          s += (k * n).to_s
+          if s == i
+            puts "%s (1..%d x %d)" % [i, k, n]
+            return
+          end
+
+          break if !i.start_with?( s )
+        end
+      end
+    end
   end
 end
 
 ProjectEuler.time do
-  # 
+  # 932718654 (1..2 x 9327)
   Problem_0038.solve()
 end
