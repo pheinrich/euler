12488ed8927f4f60f770a54db8ca69f47735a12d
Peter Heinrich <peter.heinrich@gmail.com>
2013-04-03 20:12:38 -0700

    Completed problem 59

--- solved_0059.rb
+++ solved_0059.rb
@@ -29,11 +29,33 @@ class Problem_0059
   # English words, decrypt the message and find the sum of the ASCII values in
   # the original text.
 
+  WORDS = %w(the be to of and a in that have I it for not on with he as you do
+             at this but his by from they we say her she or an will my one all
+             would there their what so up out if about who get which go me 
+             when make can like time no just him know take people into year
+             your good some could them see other than then now look only come
+             its over think also back after use two how our work first well
+             way even new want because any these give day most us)
+
   def self.solve()
+    orig = IO.read( 'resources/cipher1.txt' ).split( ',' ).map( &:to_i )
+    repeat = 1 + orig.length / 3
+
+    'aaa'.upto( 'zzz' ) do |key|
+      pad = (key * repeat).codepoints
+      plain = orig.zip( pad ).map {|pair| (pair[0] ^ pair[1]).chr}.join.split
+
+      if 15 < (WORDS & plain).length
+        puts plain.join( ' ' ).codepoints.reduce {|a, i| a + i}
+        return
+      end
+
+      key.next
+    end
   end
 end
 
 ProjectEuler.time do
-  # 
+  # 107359
   Problem_0059.solve()
 end
