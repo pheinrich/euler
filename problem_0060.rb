bdefe52e6ac22dd98d51608363b8542ad8b318cc
Peter Heinrich <peter.heinrich@gmail.com>
2013-04-27 20:36:57 -0700

    Incremental work on problem 60

--- problem_0060.rb
+++ problem_0060.rb
@@ -11,7 +11,28 @@ class Problem_0060
   # Find the lowest sum for a set of five primes for which any two primes
   # concatenate to produce another prime.
 
+  POWER = 3
+  P = ProjectEuler.eratosthenes( 10**(POWER << 1) ).map {|p| p.to_s}; nil
+  MAX = P.count {|p| POWER >= p.length } - 1
+
   def self.solve( n )
+    h = {}
+    q = P[0...MAX].map {|p| P.select {|i| i.end_with?( p )}}; nil
+
+    q.map do |a|
+      l = 1 + a[0].length
+      h[a[0]] = a[1..-1].map {|i| i[0..-l]}; nil
+    end; nil
+
+    q = h.map {|k, v| v.select {|i| h[i] && h[i].include?( k )}}; nil
+    (0...MAX-1).each do |i|
+      next if q.size < 1
+  
+      (i+1...MAX).each do |j|
+        next if (q[i] & q[j]).size < n - 1
+        puts "#{P[i]} & #{P[j]} = #{(q[i] & q[j]).size}"
+      end
+    end
   end
 end
 
