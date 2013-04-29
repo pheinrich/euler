f675556625cd312cfe33d7d652bef529dd026a09
Peter Heinrich <peter.heinrich@gmail.com>
2013-04-28 21:13:34 -0700

    Incremental implementation of problem 60

--- problem_0060.rb
+++ problem_0060.rb
@@ -12,27 +12,44 @@ class Problem_0060
   # concatenate to produce another prime.
 
   POWER = 3
-  P = ProjectEuler.eratosthenes( 10**(POWER << 1) ).map {|p| p.to_s}; nil
+  P = ProjectEuler.eratosthenes( 10**(POWER << 1) ).map {|p| p.to_s}
   MAX = P.count {|p| POWER >= p.length } - 1
 
-  def self.solve( n )
-    h = {}
-    q = P[0...MAX].map {|p| P.select {|i| i.end_with?( p )}}; nil
-
-    q.map do |a|
-      l = 1 + a[0].length
-      h[a[0]] = a[1..-1].map {|i| i[0..-l]}; nil
-    end; nil
-
-    q = h.map {|k, v| v.select {|i| h[i] && h[i].include?( k )}}; nil
-    (0...MAX-1).each do |i|
-      next if q.size < 1
-  
-      (i+1...MAX).each do |j|
-        next if (q[i] & q[j]).size < n - 1
-        puts "#{P[i]} & #{P[j]} = #{(q[i] & q[j]).size}"
+  def self.search( hash, target, array, keys )
+    array.each do |p|
+      intersection = array & hash[p]
+      if intersection.length > 0
+        break if keys.length == target
+        keys << p
+        search( hash, target, intersection, keys)
       end
     end
+
+    return keys.length == target    
+  end
+
+  def self.solve( n )
+    # For each prime < 1000, find all primes < 1000000 that end with it.  For
+    # example, q[1] = ["3", "13", "23", "43", "53", "73", "83", "103", ...].
+    q = P[0...MAX].map {|p| P.select {|i| i.end_with?( p )}}
+    h = {}
+
+    # Strip the suffix prime from each array value.  For example, the array
+    # above generates h["3"] => ["1", "2", "4", "5", "7", "8", "10", ...].
+    q.map do |a|
+      l = 1 + a[0].length
+      h[a[0]] = a[1..-1].map {|i| i[0..-l]}
+    end
+
+    # Strip the composites from each list of prefixes, and make sure the
+    # primes that remain are mutually concatenatable. Continuing the example
+    # above:  h["3"] = ["7", "11", "17", "31", "37", "59", "67", ...].
+    q = h.map {|k, v| v.select {|i| h[i] && h[i].include?( k )}}
+    h.clear
+    (0...MAX).each {|i| h[P[i]] = q[i]}
+
+    # Traverse the set looking for groups of n mutually concatenatable primes.
+    h.each {|k, v| keys = [k]; puts keys.inspect if search( h, n, v, keys ) }
   end
 end
 
