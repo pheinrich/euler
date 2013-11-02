9754de7692c3847fe240ca6b00b1b25242ffdcbc
Peter Heinrich <peter.heinrich@gmail.com>
2013-11-01 18:29:40 -0700

    Completed problem 63

--- problem_0063.rb
+++ problem_0063.rb
@@ -7,11 +7,24 @@ class Problem_0063
   #
   # How many n-digit positive integers exist which are also an nth power?
 
+  def self.count_matches(n)
+    i = 1
+    p = n
+    while p.to_s.size == i
+      p *= n
+      i += 1
+    end
+    i - 1
+  end
+
   def self.solve()
+    # 10^n will always generate a value with n+1 digits (which is too long).
+    # Therefore, count_digits(X^n) == n can only be true for X < 10.
+    puts (1..9).map {|n| count_matches(n)}.inject(:+)
   end
 end
 
 ProjectEuler.time do
-  # 
+  # 49
   Problem_0063.solve()
 end
