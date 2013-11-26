4ce74c601ef663022ad8319592e3105f7dd8060c
Peter Heinrich <peter.heinrich@gmail.com>
2013-11-25 18:31:38 -0800

    Incremental implementation of problem 60

--- problem_0060.rb
+++ problem_0060.rb
@@ -11,49 +11,61 @@ class Problem_0060
   # Find the lowest sum for a set of five primes for which any two primes
   # concatenate to produce another prime.
 
-  POWER = 3
-  P = ProjectEuler.eratosthenes( 10**(POWER << 1) ).map {|p| p.to_s}
-  MAX = P.count {|p| POWER >= p.length } - 1
+  POWER = 6
+  P = ProjectEuler.eratosthenes( 10**POWER )
 
-  def self.search( hash, target, array, keys )
-    array.each do |p|
-      intersection = array & hash[p]
-      if intersection.length > 0
-        break if keys.length == target
-        keys << p
-        search( hash, target, intersection, keys)
+  def self.check(hash, array, n, result)
+    return false if array.empty?
+    return true if 0 == n
+
+    puts "#{n}: #{array.inspect}"
+    array.each do |val|
+      break unless hash.include?( val )
+
+      isect = array & hash[val]
+      if check( hash, isect, n - 1, result )
+        result[n - 1] = val
+        return true
       end
     end
-
-    return keys.length == target    
+    false
   end
 
   def self.solve( n )
-    # For each prime < 1000, find all primes < 1000000 that end with it.  For
-    # example, q[1] = ["3", "13", "23", "43", "53", "73", "83", "103", ...].
-    q = P[0...MAX].map {|p| P.select {|i| i.end_with?( p )}}
     h = {}
+    P[0..-2].each_with_index do |p, i|
+      h[p] = []
+      pl = 1 + Math.log10( p ).to_i
 
-    # Strip the suffix prime from each array value.  For example, the array
-    # above generates h["3"] => ["1", "2", "4", "5", "7", "8", "10", ...].
-    q.map do |a|
-      l = 1 + a[0].length
-      h[a[0]] = a[1..-1].map {|i| i[0..-l]}
+      P[i+1..-1].each do |q|
+        ql = 1 + Math.log10( q ).to_i
+        break if POWER < pl + ql
+
+        x = p*(10**ql) + q
+        y = q*(10**pl) + p
+        next unless x.prime? && y.prime?
+
+        h[p] << q
+      end
     end
+    
+#    h.each do |k, v|
+#      puts "#{k}: #{v.inspect}" if 0 < v.count
+#    end
 
-    # Strip the composites from each list of prefixes, and make sure the
-    # primes that remain are mutually concatenatable. Continuing the example
-    # above:  h["3"] = ["7", "11", "17", "31", "37", "59", "67", ...].
-    q = h.map {|k, v| v.select {|i| h[i] && h[i].include?( k )}}
-    h.clear
-    (0...MAX).each {|i| h[P[i]] = q[i]}
-
-    # Traverse the set looking for groups of n mutually concatenatable primes.
-    h.each {|k, v| keys = [k]; puts keys.inspect if search( h, n, v, keys ) }
+    result = []
+    h.each do |k, v|
+      if check( h, v, n - 1, result )
+        result[n - 1] = k
+        break
+      end
+    end
+    
+    puts result.inspect
   end
 end
 
 ProjectEuler.time do
   # 
-  Problem_0060.solve( 5 )
+  Problem_0060.solve( 3 )
 end
