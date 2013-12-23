71125cbdc65e5e342aa80d252881b30de3d4a9a3
Peter Heinrich <peter.heinrich@gmail.com>
2013-12-23 00:18:30 -0800

    Updated target subdirectory in runner.rb; Advanced problem tests to most recent solutions

--- runner.rb
+++ runner.rb
@@ -1,7 +1,7 @@
 require 'projectEuler'
 
-for i in [7]
-  require "./solved/problem_%04d" % i
+for i in [76]
+  require "./problem_%04d" % i
  
   problem = Object::const_get( "Problem_%04d" % i ).new
   puts "Problem %d: %s" % [i, problem.title]
--- spec/problems.rb
+++ spec/problems.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
-n = 69
-n.upto(n) do |i|
+
+71.upto( 75 ) do |i|
   require "problem_%04d" % i
   problem = Object::const_get( "Problem_%04d" % i ).new
 
   describe problem do
-    describe ".solve" do
+    describe '.solve' do
       it problem.title do
         problem.solve.should == problem.solution
       end
