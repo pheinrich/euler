feffe7ff8d94d02104d0597e026c973b26d7db4e
Peter Heinrich <peter.heinrich@gmail.com>
2013-12-27 12:14:26 -0800

    Added 'solved' to load path

--- runner.rb
+++ runner.rb
@@ -1,7 +1,9 @@
 require 'projectEuler'
 
-for i in [76]
-  require "./problem_%04d" % i
+$LOAD_PATH.unshift File.expand_path( './../solved', __FILE__ )
+
+for i in [77]
+  require "problem_%04d" % i
  
   problem = Object::const_get( "Problem_%04d" % i ).new
   puts "Problem %d: %s" % [i, problem.title]
