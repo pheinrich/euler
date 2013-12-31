cb8f241f774707fabef9c1aff1e9a7871580f07c
Peter Heinrich <peter.heinrich@gmail.com>
2013-12-31 06:53:02 -0800

    Completed problem 85 and moved to solved

--- solved/problem_0085.rb
+++ solved/problem_0085.rb
@@ -1,9 +1,9 @@
 require 'projectEuler'
 
-# 
+# 0.04400s (12/31/13, #12247)
 class Problem_0085
   def title; 'Counting rectangles' end
-  def solution;  end
+  def solution; 2_772 end
 
   # By counting carefully it can be seen that a rectangular grid measuring 3
   # by 2 contains eighteen rectangles (see diagram).
@@ -12,5 +12,16 @@ class Problem_0085
   # million rectangles, find the area of the grid with the nearest solution.
 
   def solve( n = 2_000_000 )
+    counts = Hash.new( 0 )
+
+    # Guesstimate upper limit on grid dimensions. 
+    (1..100).each do |w|
+      (1..100).each do |h|
+        # http://www.gottfriedville.net/mathprob/comb-subrect.html
+        counts[[w, h]] = w * (w + 1) * h * (h + 1) >> 2
+      end
+    end
+
+    counts.min_by {|k, v| (n - v).abs}[0].inject( :* )
   end
 end
