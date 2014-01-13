3d84c4bec867330bcbbb6bda7ea6c886822c731c
Peter Heinrich <peter.heinrich@gmail.com>
2014-01-12 16:16:13 -0800

    Completed problem 102 and moved to solved; Added resource file for problem 102

---
+++ solved/problem_0102.rb
@@ -0,0 +1,55 @@
+require 'projectEuler'
+
+# 0.003856s (1/12/14, #10992)
+class Problem_0102
+  def title; 'Triangle containment' end
+  def solution; 228 end
+
+  # Three distinct points are plotted at random on a Cartesian plane, for
+  # which -1000 ≤ x, y ≤ 1000, such that a triangle is formed.
+  #
+  # Consider the following two triangles:
+  #
+  #      A(-340,495), B(-153,-910), C(835,-947)
+  #
+  #      X(-175,41), Y(-421,-714), Z(574,-645)
+  #
+  # It can be verified that triangle ABC contains the origin, whereas triangle
+  # XYZ does not.
+  #
+  # Using triangles.txt, a 27K text file containing the coordinates of one
+  # thousand "random" triangles, find the number of triangles for which the
+  # interior contains the origin.
+  #
+  # NOTE: The first two examples in the file represent the triangles in the
+  # example given above.
+
+  def ccw?( p1, p2 )
+    # >0 if line p1-p2 moves counterclockwise around origin
+    # =0 if line p1-p2 passes through origin
+    # <0 if line p1-p2 moves clockwise around origin
+    p1[0] * (p2[1] - p1[1]) - p1[1] * (p2[0] - p1[0]) 
+  end
+
+  def winding( verts )
+    count = 0
+    verts << verts[0]
+
+    (0..2).each do |i|
+      p, q = verts[i], verts[1 + i]
+
+      if 0 < p[1]
+        count -= 1 if 0 >= q[1] && 0 > ccw?( p, q )
+      else
+        count += 1 if 0 < q[1] && 0 < ccw?( p, q ) 
+      end
+    end
+
+    count
+  end
+
+  def solve
+    tris = IO.read( 'resources/triangles.txt' ).split( /,|\s/ ).map( &:to_i )
+    tris.each_slice( 2 ).each_slice( 3 ).count {|tri| 0 != winding( tri )}
+  end
+end
