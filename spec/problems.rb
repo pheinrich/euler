f35bc79482a46abc218c05092018166828e67e3f
Peter Heinrich <peter.heinrich@gmail.com>
2015-01-11 14:18:24 -0800

    Updated spec/problems.rb to eliminate deprecation warnings

--- spec/problems.rb
+++ spec/problems.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
-1.upto( 87 ) do |i|
+81.upto( 83 ) do |i|
   require "problem_%04d" % i
   problem = Object::const_get( "Problem_%04d" % i ).new
 
   describe "Problem %d: %s" % [i, problem.title] do
     describe '.solve' do
       it problem.solution do
-        problem.solve.should == problem.solution
+        expect( problem.solve ).to  eq( problem.solution )
       end
     end
   end
