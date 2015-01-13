15103070f0554f497dab9a518cacac02f7c23656
Peter Heinrich <peter.heinrich@gmail.com>
2015-01-13 01:06:23 -0800

    Completed problem 113 and moved to solved

---
+++ solved/problem_0113.rb
@@ -0,0 +1,45 @@
+require 'projectEuler'
+
+# 0.001681s (1/13/15, #6664)
+class Problem_0113
+  def title; 'Non-bouncy numbers' end
+  def solution; 51_161_058_134_250 end
+
+  # Working from left-to-right if no digit is exceeded by the digit to its
+  # left it is called an increasing number; for example, 134468.
+  #
+  # Similarly if no digit is exceeded by the digit to its right it is called
+  # a decreasing number; for example, 66420.
+  #
+  # We shall call a positive integer that is neither increasing nor decreasing
+  # a "bouncy" number; for example, 155349.
+  #
+  # As n increases, the proportion of bouncy numbers below n increases such
+  # that there are only 12951 numbers below one-million that are not bouncy
+  # and only 277032 non-bouncy numbers below 10^10.
+  #
+  # How many numbers below a googol (10^100) are not bouncy?
+
+  def solve( log10 = 100 )
+    total = 0
+
+    # Sum increasing number counts for each digit position. This calculation
+    # doesn't distinguish numbers that are flat (e.g. '111', '999', '44444').
+    digcnt = Array.new( 9 ) {1}
+    log10.times do
+      total += digcnt.reduce( :+ )
+      (0...9).each {|i| digcnt[i] = digcnt[i..-1].reduce( :+ )}
+    end
+
+    # Update total with counts for decreasing numbers. Flat numbers are count-
+    # ed here, too, so we'll account for that later.
+    digcnt = Array.new( 9 ) {1}
+    log10.times do
+      total += digcnt.reduce( :+ )
+      (0...9).each {|i| digcnt[i] = 1 + digcnt[i..-1].reduce( :+ )}
+    end
+
+    # Adjust total to avoid double-counting the flats.
+    total - 9*log10
+  end
+end
