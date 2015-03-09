485e57ab858170a56b2e18d15428b9e4844e672b
Peter Heinrich <peter.heinrich@gmail.com>
2015-03-09 12:20:59 -0700

    Additional placeholders

---
+++ problem_0162.rb
@@ -0,0 +1,37 @@
+require 'projectEuler'
+
+# 
+class Problem_0162
+  def title; 'Hexadecimal numbers' end
+
+  # In the hexadecimal number system numbers are represented using 16 diff-
+  # erent digits:
+  #
+  #      0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F
+  #
+  # The hexadecimal number AF when written in the decimal number system equals
+  # 10x16+15=175.
+  #
+  # In the 3-digit hexadecimal numbers 10A, 1A0, A10, and A01 the digits 0,1
+  # and A are all present. Like numbers written in base ten we write hexa-
+  # decimal numbers without leading zeroes.
+  #
+  # How many hexadecimal numbers containing at most sixteen hexadecimal digits
+  # exist with all of the digits 0,1, and A present at least once? Give your
+  # answer as a hexadecimal number.
+  #
+  # (A,B,C,D,E and F in upper case, without any leading or trailing code that
+  # marks the number as hexadecimal and without leading zeroes, e.g. 1A3F and
+  # not: 1a3f and not 0x1a3f and not $1A3F and not #1A3F and not 0000001A3F)
+
+  def refs; [] end
+  def solution; end
+  def best_time; end
+
+  def completed_on; '2015-01-21' end
+  def ordinality; end
+  def percentile; end
+
+  def solve( n )
+  end
+end
---
+++ problem_0178.rb
@@ -0,0 +1,26 @@
+require 'projectEuler'
+
+# 
+class Problem_0178
+  def title; 'Step Numbers' end
+
+  # Consider the number 45656.
+  #
+  # It can be seen that each pair of consecutive digits of 45656 has a diff-
+  # erence of one. A number for which every pair of consecutive digits has a
+  # difference of one is called a step number. A pandigital number contains
+  # every decimal digit from 0 to 9 at least once.
+  #
+  # How many pandigital step numbers less than 10^40 are there?
+
+  def refs; [] end
+  def solution; end
+  def best_time; end
+
+  def completed_on; '2015-01-21' end
+  def ordinality; end
+  def percentile; end
+
+  def solve( n )
+  end
+end
