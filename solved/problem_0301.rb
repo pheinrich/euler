c5a5a70e65830a3588f89f8068e075985975d5cb
Peter Heinrich <peter.heinrich@gmail.com>
2016-06-01 12:08:42 -0700

    Completed problem 301 and moved to solved

--- solved/problem_0301.rb
+++ solved/problem_0301.rb
@@ -1,6 +1,6 @@
 require 'projectEuler'
 
-# 
+# 0.00001502s (5/31/16, #3874)
 class Problem_0301
   def title; 'Nim' end
 
@@ -18,31 +18,40 @@ class Problem_0301
   # and n3 then there is a simple function X(n1,n2,n3) — that you may look up
   # or attempt to deduce for yourself — that returns:
   #
-  #   * zero if, with perfect strategy, the player about to move will event-
+  #   - zero if, with perfect strategy, the player about to move will event-
   #     ually lose; or
-  #   * non-zero if, with perfect strategy, the player about to move will
+  #   - non-zero if, with perfect strategy, the player about to move will
   #     eventually win.
   #
   # For example X(1,2,3) = 0 because, no matter what the current player does,
   # his opponent can respond with a move that leaves two heaps of equal size,
-  # at which point every move by the current player can be mirrored by his
-  # opponent until no stones remain; so the current player loses. To illu-
-  # strate:
+  # at which point every move by the current player can be mirrored by his op-
+  # ponent until no stones remain; so the current player loses. To illustrate:
+  #
   #   - current player moves to (1,2,1)
   #   - opponent moves to (1,0,1)
   #   - current player moves to (0,0,1)
   #   - opponent moves to (0,0,0), and so wins.
   #
-  # For how many positive integers n ≤ 2^30 does X(n,2n,3n) = 0 ?
+  # For how many positive integers n ≤ 230 does X(n,2n,3n) = 0 ?
 
-  def refs; [] end
-  def solution; end
-  def best_time; end
+  def refs; ["https://en.wikipedia.org/wiki/Nim"] end
+  def solution; 2_178_309 end
+  def best_time; 0.00001502 end
 
-  def completed_on; '2013-01-21' end
-  def ordinality; end
-  def population; end
+  def completed_on; '2016-05-31' end
+  def ordinality; 3_874 end
+  def population; 566_837 end
 
-  def solve( n )
+  def solve( exp = 30 )
+    # According to Wikipedia, in normal Nim play, "the winning strategy is to
+    # finish every move with a nim-sum of 0" (where nim-sum is the XOR of all
+    # heap sizes). The pattern is easily identified for n = 2^exp:
+    #
+    #   exp     0    1    2    3    4    5    6    7    8    9   10
+    #   count   1    2    3    5    8   13   21   34   55   89  144
+    #
+    # (count computed as the number of i ≤ n where i^(2*i)^(3*i) == 0.)
+    (exp + 2).fib
   end
 end
