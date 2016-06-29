6c7af915ad0fe683f6eb9b09deb217f3dba9f8b4
Peter Heinrich <peter.heinrich@gmail.com>
2016-06-28 19:27:12 -0700

    Working prototype, but slow

--- problem_0126.rb
+++ problem_0126.rb
@@ -25,52 +25,54 @@ class Problem_0126
   #
   # Find the least value of n for which C(n) = 1000.
 
-  def cover( x, y, z, n )
-    total  = 2 * (x*y + y*z + z*x)
-    total += (n << 2) * (x + y + z)
-    total += (n*n - n) << 2
-  end
-  
   def cover2( x, y, z, n )
     2*(x*y + y*z + z*x) + 4*n*(x + y + z) + 4*n*(n - 1)
-    
-    # Given any MIN < x, y, z, n < MAX, what are all possible values for the
-    # expression above?
-    #
-    # 2 * [(xy + yz + zx) + 2n(x + y + z) + 2n(n - 1)] =
-    # 2 * [(xy + yz + zx) + 2n[(x + y + z) + (n - 1)]] =
-    # 2 * [(xy + yz + zx) + 2n(x + y + z + n - 1)] =
-    # 2 * [xy + yz + zx + 2nx + 2ny + 2nz + 2nn - 2n] =
-    # 2 * [2nx + xy + 2ny + yz + 2nz + zx + 2nn - 2n] =
-    # 2 * [x(2n + y) + y(2n + z) + z(2n + x) + n(2n + 2)]
   end
 
-  def cover3( x, y, z, max, memo )
-    a = (x*y + y*z + z*x) << 1
-    b = (x + y + z) << 2
-    
-    (0..max).each do |n|
-      t = a + n*b + ((n*n - n) << 2)
-#      puts "(#{x}, #{y}, #{z}) = #{t} (level #{n})"
-      memo[t] += 1
+  def c_of_n( n )
+    total = 0
+    x, y, z, d = 1, 1, 1, 0
+
+    sum = (x*y + y*z + z*x) << 1
+    while sum <= n
+      total += 1 if sum == n
+
+      sum += (x + y + z + 2*d) << 2
+      d += 1
+      
+      if sum > n
+        z += 1
+        d = 0
+        sum = (x*y + y*z + z*x) << 1
+        
+        if sum > n
+          y += 1
+          z = y
+          sum = (x*y + y*z + z*x) << 1
+          
+          if sum > n
+            x += 1
+            y = x
+            z = y
+            sum = (x*y + y*z + z*x) << 1
+          end
+        end
+      end
     end
-  end
-
-  def recurse( sum, memo )
-    # Recursively solve the Diophantine equation that describes the number of
-    # blocks needed to cover a cuboid of dimensions x, y, z to a depth of n:
+    
+    total
   end
 
   # From the problem description provided, it's clear that the number of cubes
   # necessary to cover a cuboid will initially equal its surface area. This
   # value grows as a function of the number of layers. To cover a cuboid with
-  # dimensions x, y, z to a depth of n, N blocks will be necessary:
+  # dimensions x, y, z to a depth of d, N blocks will be necessary:
   #
-  #   N = 2(xy + yz + zx) + 4n(x + y + z) + 4n(n - 1)
+  #   N = 2(xy + yz + zx) + 4d(x + y + z) + 4d(d - 1)
   #
   # The problem is to find the lowest N with exactly 1,000 unique solutions
-  # [x, y, z, n], with x, y, z >= 1, n >= 0, all integers. We could try a
-  # brute-force search (calculate N for all values of x, y, z, n in some
+  # [x, y, z, d], with x, y, z >= 1, d >= 0, all integers. We could try a
+  # brute-force search (calculate N for all values of x, y, z, d in some
   # range) and count collisions, but it's unclear how we would limit the
   # search.
   #
@@ -80,26 +82,22 @@ class Problem_0126
   # only one variable, which we can then solve for. How we choose "reasonable"
   # values and knowing when to stop depend on the equation.
   #
-  # Assume we are going to supply x, y, and n, then solve for z. In that case,
+  # We can infer several facts from the equation above:
   #
-  #   N = 2(xy + yz + zx) + 4n(x + y + z) + 4n(n - 1)
-  #     = 2xy + 2yz + 2zx + 4nx + 4ny + 4nz + 4n^2 - 4n
-  #     = (2xy + 4nx + 4ny + 4n^2 - 4n) + (2yz + 2zx + 4nz)
-  #   N - (2xy + 4nx + 4ny + 4n^2 - 4n) = z(2x + 2y + 4n)
-  #   z = [N - (2xy + 4nx + 4ny + 4n^2 - 4n)] / (2x + 2y + 4n)
-  #     = N/2 - xy - 2nx - 2ny - 2n^2 + 2n / (x + y + 2n)
-  #
-  # From this we can infer:
-  #
-  #   1) N/2 - xy - 2nx - 2ny - 2n^2 + 2n must be a positive integer
-  #   2) All of the terms in 1) must be integers
-  #   3) N/2, being an integer, implies that N is even
-  #   4) Since x, y >= 1, the minimum value of (x + y + 2n) is 2
-  #   5) N/2 - xy - 2nx - 2ny - 2n^2 + 2n >= x + y + 2n >= 2
-  #   6) N/2 >= xy + 2nx + 2ny + 2n^2 - 2n + 2 => N >= 6
-  #   7) N/2 - xy - 2nx - 2ny - 2n^2 + 2n is a multiple of x + y + 2n
+  #   1) N >= 2(xy + yz + zx), N >= 4d(x + y + z), N >= 4d(d - 1)
+  #   2) N/2 - xy - 2dx - 2dy - 2d^2 + 2d must be a positive integer
+  #   3) All of the terms in 1) must be integers
+  #   4) N/2, being an integer, implies that N is even
+  #   5) Since x, y >= 1, the minimum value of (x + y + 2d) is 2
+  #   6) N/2 - xy - 2dx - 2dy - 2d^2 + 2d >= x + y + 2d >= 2
+  #   7) N/2 >= xy + 2dx + 2dy + 2d^2 - 2d + 2 => N >= 6
+  #   8) N/2 - xy - 2dx - 2dy - 2d^2 + 2d is a multiple of x + y + 2d
   # 
-  def solve( n = 1_000 )
+  def solve( c = 100 )
+    n = 6
+    n += 2 until c == c_of_n( n )
+    puts "c_of_n( #{n} ) = #{c_of_n( n )}"
+    n 
   end
 
   def solution; end
