9dd6f30df9ddecfc25bc694a071ac49b49fcf78e
Peter Heinrich <peter.heinrich@gmail.com>
2016-07-01 12:18:51 -0700

    Initial implementation

--- problem_0128.rb
+++ problem_0128.rb
@@ -29,7 +29,104 @@ class Problem_0128
   #
   # Find the 2000th tile in this sequence.
 
+  def fill_coords( max, map )
+    # * = [0, 0]
+    # {*} -> N
+    # {*} -> {SW} -> {S} -> {SE} -> {NE} -> {N} -> NW -> N
+    # {*} -> {SW} -> {SW} -> {S} -> {S} -> {SE} -> {SE} -> {NE} -> {NE} -> {N} -> {N} -> {NW} -> NW -> N
+    # {*} -> {SW} -> {SW} -> {SW} -> {S} -> {S} -> {S} -> {SE} -> {SE} -> {SE} -> {NE} -> {NE} -> {NE} -> {N} -> {N} -> {N} -> {NW} -> {NW} -> NW -> N
+    map[[0, 0]] = 1
+      
+    x, y = 0, -1
+    n = 2
+    run = 1
+    
+    while n < max
+      map[[x, y]] = n
+
+      # Southwest
+      run.times do
+        x -= 1
+        y += 1
+        n += 1
+        map[[x, y]] = n
+      end
+      
+      # South
+      run.times do
+        y += 1
+        n += 1
+        map[[x, y]] = n
+      end
+      
+      # Southeast
+      run.times do
+        x += 1
+        n += 1
+        map[[x, y]] = n
+      end
+
+      # Northeast
+      run.times do
+        x += 1
+        y -= 1
+        n += 1
+        map[[x, y]] = n
+      end
+      
+      # North
+      run.times do
+        y -= 1
+        n += 1
+        map[[x, y]] = n
+      end
+      
+      # Northwest
+      (run - 1).times do
+        x -= 1
+        n += 1
+        map[[x, y]] = n
+      end
+      
+      n += 1
+      x -= 1
+      y -= 1
+      run += 1
+    end
+
+    # Return the number of complete rings.
+    return (y + 2).abs
+  end
+
+  def fill_diffs( rings, coords, diffs )
+    (-rings..rings).each do |x|
+      (-rings..rings).each do |y|
+        next unless (x + y).abs <= rings
+ 
+        n = coords[[x, y]]
+        pd = 0
+        
+        pd += 1 if (n - coords[[x, y - 1]]).abs.prime?
+        pd += 1 if (n - coords[[x - 1, y]]).abs.prime?
+        pd += 1 if (n - coords[[x - 1, y + 1]]).abs.prime?
+        pd += 1 if (n - coords[[x, y + 1]]).abs.prime?
+        pd += 1 if (n - coords[[x + 1, y]]).abs.prime?
+        pd += 1 if (n - coords[[x + 1, y - 1]]).abs.prime?
+
+#        puts "#{n}:[#{x}, #{y}] -> #{pd}"
+        diffs << n if 3 == pd
+      end
+    end
+  end
+
   def solve( n = 2_000 )
+    coords = {}
+    rings = fill_coords( 5000000, coords )
+
+    diffs = []
+    fill_diffs( rings, coords, diffs )
+    
+    puts diffs.count
   end
 
   def solution; end
@@ -40,5 +137,7 @@ class Problem_0128
   def ordinality; end
   def population; end
 
-  def refs; [] end
+  def refs
+    ["http://www.redblobgames.com/grids/hexagons/#coordinates"]
+  end
 end
