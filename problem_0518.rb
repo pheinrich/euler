c6ca959dce47f16413318e5f47b14f478b79d583
Peter Heinrich <peter.heinrich@gmail.com>
2018-09-21 10:46:03 -0700

    Added problem 518 template

---
+++ problem_0518.rb
@@ -0,0 +1,33 @@
+require 'projectEuler'
+
+class Problem_0518
+  def title; 'Prime triples and geometric sequences' end
+  def difficulty; 20 end
+
+  # Let S(n) = Σ a+b+c over all triples (a,b,c) such that:
+  #
+  #   a, b, and c are prime numbers.
+  #   a < b < c < n.
+  #   a+1, b+1, and c+1 form a geometric sequence.
+  #
+  # For example, S(100) = 1035 with the following triples:
+  #
+  #   (2, 5, 11), (2, 11, 47), (5, 11, 23), (5, 17, 53), (7, 11, 17),
+  #   (7, 23, 71), (11, 23, 47), (17, 23, 31), (17, 41, 97), (31, 47, 71),
+  #   (71, 83, 97)
+  #
+  # Find S(10^8).
+
+  def solve( n = 100_000_000 )
+  end
+
+  def solution; '' end
+  def best_time; 1 end
+  def effort; 20 end
+
+  def completed_on; '20??-??-??' end
+  def ordinality; 1 end
+  def population; 1 end
+
+  def refs; [] end
+end
