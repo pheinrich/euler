d52f8c36d0bb334444d45bd3e0b518fe3e01b52d
Peter Heinrich <peter.heinrich@gmail.com>
2018-09-25 15:39:47 -0700

    Updated problem template to not fail empty

--- template_0000.rb
+++ template_0000.rb
@@ -2,20 +2,20 @@ require 'projectEuler'
 
 class Problem_0000
   def title; '' end
-  def difficulty; end
+  def difficulty; 10 end
 
   # 
 
   def solve( n )
   end
 
-  def solution; end
-  def best_time; end
-  def effort; end
+  def solution; '' end
+  def best_time; 1 end
+  def effort; 10 end
 
   def completed_on; '20??-??-??' end
-  def ordinality; end
-  def population; end
+  def ordinality; 1 end
+  def population; 1 end
 
   def refs; [] end
 end
