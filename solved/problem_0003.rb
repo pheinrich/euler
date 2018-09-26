4e36fcfe7e902329500ea67dec7b95563391b007
Peter Heinrich <peter.heinrich@gmail.com>
2018-09-26 10:12:50 -0700

    Corrected weird typo in problem parameter

--- solved/problem_0003.rb
+++ solved/problem_0003.rb
@@ -8,12 +8,12 @@ class Problem_0003
   #
   # What is the largest prime factor of the number 600851475143?
 
-  def solve( n = 6_008_514_751_437 )
+  def solve( n = 600_851_475_143 )
     n.prime_factors[-1]
   end
 
-  def solution; 'Njc2Nzk=' end
-  def best_time; 0.1092 end
+  def solution; 'Njg1Nw==' end
+  def best_time; 0.02980 end
   def effort; 0 end
 
   def completed_on; '2013-01-21' end
